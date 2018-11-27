f170fe590878d20f81ce6b6a282801d2bc158137
Long Tran <nhulongctk10@gmail.com>
2018-11-27 11:32:43 +0700

    config webpack cli

--- components/app.js
+++ components/app.js
@@ -1,8 +1,15 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return <h2>Hello React App</h2>;
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Staring ReactJS</h2>
+        <p>git remote add origin git@github.com:nhulongctk35/react-ssr.git</p>
+      </React.Fragment>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
