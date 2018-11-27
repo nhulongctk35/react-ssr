2f5e3372668937164e7d314e72a2c9c2e4edbdf9
Long Tran <nhulongctk10@gmail.com>
2018-11-27 09:55:44 +0700

    First commit

---
+++ components/app.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom')
+
+function App() {
+    return (
+        <h2>Hello React App</h2>
+    )
+};
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: {
+    app: "./components/app.js"
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  }
+};
