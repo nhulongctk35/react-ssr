78d4ceedfaac5da95ed7af4545ce6cf0accb1eee
Long Tran <nhulongctk10@gmail.com>
2018-11-29 11:20:05 +0700

    using pm2 babel-node

--- components/app.js
+++ components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ArticleList from 'articles/article-list';
+import ArticleList from './articles/article-list';
 
 class App extends React.Component {
   constructor(props) {
